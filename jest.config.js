ff8c862a4587f15950a7b23200e8c9d2b52ec102
Manuel Fernandez <manferlo81@gmail.com>
2019-07-01 11:37:15 +0700

    config cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,3 @@
-const CI = process.env.CI;
 const threshold = 95;
 
 module.exports = {
@@ -13,7 +12,7 @@ module.exports = {
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageReporters: [
-    CI ? "json" : "lcov",
+    process.env.CI ? "json" : "lcov",
     "text",
     "text-summary",
   ],
